b1ead66cff3e375435a65c78e1d623604a5f5126
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-06-09 23:34:11 +0200

    Add organization logo

--- app/controllers/layout.js
+++ app/controllers/layout.js
@@ -1,3 +1,4 @@
 angular.module('mainApp').controller('mainCtrl', function ($scope, statistics) {
 	$scope.statistics = statistics;
+	$scope.organizations = _.filter(statistics.organizations, 'featured');
 });
